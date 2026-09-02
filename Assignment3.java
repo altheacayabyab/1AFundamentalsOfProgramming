e7e1226c6ddae788bd8435437a0d3bad97b2f9c7
Althea Rose Cayabyab <altheacayabyab85@gmail.com>
2026-09-02 20:54:45 +0800

    Add Assignment 3

---
+++ Assignment3.java
@@ -0,0 +1,18 @@
+public class Assignment3 {
+	public static void main(String[] args) {
+		
+		String firstname = "Althea Rose";
+		String lastname = "Cayabyab";
+		String address = "Calasiao, Pangasinan";
+		String mobileNumber = "09064561484";
+		int age = 19;
+		double gwa = 94.43;
+		
+		System.out.println("Firstname: " + firstname);
+		System.out.println("Lastname: " + lastname);
+		System.out.println("Address: " + address);
+		System.out.println("Mobile Number: " + mobileNumber);
+		System.out.println("Age: " + age);
+		System.out.println("GWA: " + gwa);
+	}
+}
